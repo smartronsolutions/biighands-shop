c8ba2ab8621a26aa87c6ddcd998e3b9029609851
smartronsolutions <support@smartronsolutions.co.uk>
2026-06-19 20:46:38 +0500

    1

---
+++ biighands_shop_website/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Biighands Shop Website',
+    'summary': 'Screwfix-style eCommerce homepage for Biighands Shop',
+    'version': '19.0.1.0',
+    'category': 'Website/eCommerce',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'views/home.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
