d1b3701ad19694fdb7a852ad2ffeef1a8c3c75b5
smartronsolutions <support@smartronsolutions.co.uk>
2026-08-25 03:17:56 +0500

    h

--- biighands_shop_website/__manifest__.py
+++ biighands_shop_website/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Playground Website',
-    'version': '19.0.4.0.31',
+    'version': '19.0.4.0.32',
     'category': 'Website',
     'depends': ['website_sale'],
     'data': [
